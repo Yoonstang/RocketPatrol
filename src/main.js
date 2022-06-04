d1996b1a8689661cb63a9976b649c6884fdbb584
spencer <ykim156@ucsc.edu>
2022-06-03 22:53:07 -0700

    what i've done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 /* Spencer Kim 
 Rocket Partrol Mods
 6/2/22
-took about 17hrs so...
+took about 20hrs so...
+
+I think i did Implement a simultaneous two-player mode (30)
+Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (20) 
+Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)
+Allow the player to control the Rocket after it's fired (5)
+Add your own (copyright-free) background music to the Play scene (5)
+
 */
 
 let config = {
